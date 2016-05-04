78d0ff32d4588bf187c3fc5ac00021ed221969e5
tongc <chentongtc@gmail.com>
2016-05-04 10:39:56 -0400

    Update versioned.py

--- versioned.py
+++ versioned.py
@@ -4,14 +4,16 @@ class VersionedCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		#save the file in case if there is any modifications before start versioning it.
 		sublime.active_window().run_command("save")
+		os.system("cd " + self.currentDir())
+		gitStatus = os.popen('git status').read()
+		if("Not a git repository" not in gitStatus):
+			os.system("cd " + self.currentDir() + " & git init . & git add . & git commit -m 'modified'")
+		else:
+			os.system("cd " + self.currentDir() + " & git add . & git commit -m 'modified'")
+	def currentDir(self):
 		file_name=self.view.file_name()
 		path=file_name.split("\\")
 		current_driver=path[0]
 		path.pop()
-		current_directory="\\".join(path)
-		os.system("cd " + current_directory)
-		gitStatus = os.popen('git status').read()
-		if("Not a git repository" not in gitStatus):
-			os.system("cd " + current_directory + " & git init . & git add . & git commit -m 'modified'")
-		else:
-			os.system("cd " + current_directory + " & git add . & git commit -m 'modified'")
+		current_directory="\\".join(path)		
+		return current_directory
